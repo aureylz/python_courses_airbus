34037bd17ca17d20017d3938eba661188092d9fb
poveda_g <guillaume.poveda@airbus.com>
2022-07-08 10:20:18 +0200

    correct manon exercises

--- python/03_python-advanced/3-functional-programming/correction/course/compose_reduce_map.py
+++ python/03_python-advanced/3-functional-programming/correction/course/compose_reduce_map.py
@@ -1,2 +1,3 @@
 def map_reduce(f_map, f_reduce):
-       return lambda x: my_reduce_list((my_map(x, len(x),f_map)),f_reduce,None)
+    return compose(lambda y: reduce(f_reduce, y), 
+                   lambda x: map(f_map, x))
--- python/03_python-advanced/3-functional-programming/correction/course/my_reduce.py
+++ python/03_python-advanced/3-functional-programming/correction/course/my_reduce.py
@@ -1,6 +1,12 @@
-def my_reduce_list(list_: List, function, initializer):
-    if len(list_) == 1:   
-        return  list_[0]                  
-    else: 
-        result = function(list_[0], my_reduce_list(list_[1:],function,initializer))
-        return result
+def my_reduce_list(list_, function, initializer):
+    if len(list_)==0:
+        return initializer
+    if len(list_)==1:
+        if initializer is None:
+            return None
+        else:
+            return function(initializer, list_[0])
+    if initializer is None:
+        return my_reduce_list(list_[2:], function, function(list_[0], list_[1]))
+    else:
+        return my_reduce_list(list_[1:], function, function(initializer, list_[0]))
