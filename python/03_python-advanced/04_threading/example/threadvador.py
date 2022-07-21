318ab6be079a4f17acaa6929d7d17b97dd981797
Olivier <olivier.etienne@airbus.com>
2022-07-21 20:02:20 +0200

    threading first draft

---
+++ python/03_python-advanced/04_threading/example/threadvador.py
@@ -0,0 +1,17 @@
+import threading
+import time
+
+def thread_function(ttl):
+    name = threading.current_thread().getName()
+    print('[Thread %s] starts' % (name))
+    time.sleep(ttl)
+    print('[Thread %s] diiied' % name)
+
+if __name__ == "__main__":
+    print("[Thread Main] start ---")
+    luke = threading.Thread(target=thread_function, name="luke", args=(5,))
+    darthvader = threading.Thread(target=thread_function, name="darthvader", args=(10,))
+    luke.start()
+    darthvader.start()
+
+    print("[Thread Main] end -----")
