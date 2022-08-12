41b7d5071728ee40afd689e33be9909afbffa12c
Micael Civray <micael.civray@airbus.com>
2022-08-12 17:13:00 +0200

    adding tp2 for basics

--- python/02_python-basics/labs/tp2_solution.py
+++ python/02_python-basics/labs/tp2_solution.py
@@ -1,97 +1,103 @@
 from datetime import date
 from calendar import month_name
+import sys
 
-# 01 - Days of the week
+# 02 - Days of the week
 
-# 1.1 Days
+# 2.1 Days
 
-# 1.1.1
+# 2.1.1
 days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
 
-# 1.1.2 (example for Wednesday)
+# 2.1.2 (example for Wednesday)
 days[2]
 
-# 1.1.3
+# 2.1.3
 for idx, day in enumerate(days):
     print(f"day {idx+1} of the week is {day}")
 
-# 1.1.4
+# 2.1.4
 for day in days:
     print(day[:2])
 
-# 1.1.5
+# 2.1.5
 new_day = ''
 for day in days:
     new_day += day[5]
 
-# 1.1.6
+# 2.1.6
 days.append('yasdydy')
 
-# 1.1.7
+# 2.1.7
 days.remove('yasdydy')  # or del(days[7]) or days.pop(7)
 
-# 1.2 Days & mood
+# 2.2 Days & mood
 
-# 1.2.1
+# 2.2.1
 days_moods = {days[0]: "Starting the week slowly", days[1]: "Focus on my studies", days[2]: "Looking for a new TV show",
               days[3]: "Time to have a beer in the city center", days[4]: "Preparing the week-end", days[5]: "Ready for the party!", days[6]: "Studying for tomorrow's  exams"}
 
-# 1.2.2 (example for Wednesday)
+# 2.2.2 (example for Wednesday)
 days_moods['Wednesday']
 
-# 1.2.3
+# 2.2.3
 days_moods['Monday'] = "Starting the week by an exam"
 
-# 1.2.4
+# 2.2.4
 days_moods.popitem()  # or days_moods.pop('Sunday') or del(days_moods['Sunday'])
 
-# 1.2.5
+# 2.2.5
 days_moods.update({'Sunday': 'Relaxing before holidays'})  # or days_moods['Sunday'] = 'Relaxing before holidays'
 
-# 1.2.6
+# 2.2.6
 shortdays_moods = {day[:3]: mood.split()[0] for day, mood in days_moods.items()}
 
-# 1.3 What day is it?
+# 2.3 What day is it?
 
-# 1.3.1
+# 2.3.1
 # Cf import at the top
 
-# 1.3.2
+# 2.3.2
 f"Today we are {days[date.today().isoweekday()-1]} {date.today().day}"
 
-# 1.3.3
+# 2.3.3
 
 
-def order(arg):
+def order(nb):
     '''
     Convert integer to ordinal number.  
     Args:
-    -  arg: integer (eg: 1)
+    -  nb: integer (eg: 1)
     Returns:
     - ordinal number (eg: 1st)
     '''
-    # Convert to integer (allow string or float arg if value is correct, else raise an error)
-    i = int(arg)
-    if arg < 1 or arg > 99:
+    # Convert to integer (allow string or float nb if value is correct, else raise an error)
+    nb = int(nb)
+    if nb < 1 or nb > 99:
         raise ValueError('Number is expected to be > 0 and < 100')
     # 1 => 1st, 2 => 2nd, 3 => 3rd, except 11th 12th 13th
-    if 11 <= i <= 13:
-        return str(i)+'th'
-    if i % 10 == 1:
-        return str(i)+'st'
-    if i % 10 == 2:
-        return str(i)+'nd'
-    if i % 10 == 3:
-        return str(i)+'rd'
+    if 11 <= nb <= 13:
+        return str(nb)+'th'
+    if nb % 10 == 1:
+        return str(nb)+'st'
+    if nb % 10 == 2:
+        return str(nb)+'nd'
+    if nb % 10 == 3:
+        return str(nb)+'rd'
     else:
-        return str(i)+'th'
+        return str(nb)+'th'
 
 
-# 1.3.4
+# 2.3.4
 f"Today we are {days[date.today().isoweekday()-1]}, the {order(date.today().day)}"
 
-# 1.3.5
+# 2.3.5
 f"Today we are {days[date.today().isoweekday()-1]}, the {order(date.today().day)} of {month_name[date.today().month]}"
 
-# 1.3.6
+# 2.3.6
+if __name__ == "__main__":
+    # print(sys.argv, len(sys.argv), sys.argv)
+    print(f"ordinal of {sys.argv[1]} is {order(sys.argv[1])}")
+
+# 2.3.7
 f"Today we are {date.today().strftime('%A')}, the {order(date.today().day)} of {date.today().strftime('%B')}"
