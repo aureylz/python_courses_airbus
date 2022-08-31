228a2f49461681329b3d5593433753b93b3baa19
poveda_g <guillaume.poveda@airbus.com>
2022-08-31 16:26:58 +0200

    delete last parts of the course, added new example for map function

--- python/03_python-advanced/03_functional_programming/correction/course/my_map.py
+++ python/03_python-advanced/03_functional_programming/correction/course/my_map.py
@@ -1,10 +1,9 @@
 from typing import List, Iterable
-def my_map(list_: List, len_list: int, function)->List:
-    if len_list == 0:
-        return []
-    else:
-        return [function(list_[0])]+my_map(list_[1:], len_list-1, function)
-def my_map_iterable(iterable: Iterable, function)->Iterable:
-    for item in iterable:
-        yield function(item)
+def my_map(list_: List, function)->List:
+    def my_map_len(input_list, len_input_list, function):
+        if len_input_list == 0:
+            return []
+        else:
+            return [function(input_list[0])]+my_map_len(input_list[1:], len_input_list-1, function)
+    return my_map_len(list_, len(list_), function)
 
