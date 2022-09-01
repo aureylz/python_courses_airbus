50e487182820b589c8a90cbb325c95df99b05b4d
bondouy_m <manon.bondouy@airbus.com>
2022-09-01 16:54:35 +0200

    update

---
+++ python/03_python-advanced/03_functional_programming/correction/course/check_vowels.py
@@ -0,0 +1,9 @@
+def check_vowels(letter):
+  list_of_vowels = ['a', 'e', 'i', 'o', 'u']
+  if letter in list_of_vowels:
+      return True
+  else:
+      return False
+letters = ['t', 'e', 'm', 'c', 'i', 'd', 'z', 'p', 'o']
+filtered_object = filter(check_vowels, letters)
+print(list(filtered_object))
