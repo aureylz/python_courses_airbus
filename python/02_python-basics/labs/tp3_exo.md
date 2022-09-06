bb5d28399fd51ec1372ca637f4c0689a8eb0d1f9
Sebastien Arfort <sebastien.arfort@airbus.com>
2022-09-06 16:15:47 +0200

    merge micael and seb Python basics / Lab 3

--- python/02_python-basics/labs/tp3_exo.md
+++ python/02_python-basics/labs/tp3_exo.md
@@ -1,6 +1,6 @@
 # TP N°3 (Python Basics)
 
-# 3.1 Functions
+## 3.1 Functions
 
 - 3.1.1 - Use one of the datetime module functions to displays the current time in your zone
 - 3.1.2 - Wrap the above code into a new function to display the current time in your zone
@@ -8,10 +8,36 @@
 - 3.1.4 - Modify again the function to accept a time formatter to customize the time display format
 - 3.1.5 - Now make the parameters optional, if not provided, display time in your current zone
 
-# Control Flow
+## 3.2 Control Flow
+
 - 3.2.1 - Create a function that displays the current time during 1 mn and refresh every second
 - 3.2.2 - Modify the above function so that it beeps every even seconds
 - 3.2.3 - Create a countdown time that displays a * each second, but starts displaying the 10 remaining seconds
 
-# Files
+## 3.3 Files
+
 - 3.3.1 - Create a function that accepts an arbitrary number of key / value parameters and save it as json
+- 3.3.2 - Read csv file 'tp3_airbus_family.csv', print lines describing Airbus aircrafts.  
+  eg: ```['A220-100', '100-120', '3450']```
+- 3.3.3 Read again csv file 'tp3_airbus_family.csv', print each line as a json.  
+  eg: ```{'name':'A220-100', 'seating':'100-120', 'range':'3450' }```
+- 3.3.4 Save read data from csv file to a new json file.  File content:
+  ```json
+  [  {"name": "A220-100", "seating": "100-120", "range": "3450"}, ... ]
+  ```
+
+## 3.4 Unit tests
+
+- 3.4.1 Write the code above into a function: 'aircrafts_csv_to_json(csvin, jsonout)'.  
+- 3.4.2 Write a unit test that will:
+  - execute the function to create a json file
+  - check the content of the file: read json file and check it contains the expected list 
+  - delete the json file.
+- 3.4.3 Execute the unit test from a main function.  
+  `python3 tp3_script.py`
+- 3.4.4 Execute test with pytest:
+  - remove the previous __main__ test
+  - `pip install pytest` if not done
+  - `pytest tp3_script.py`
+- 3.4.5 Change expected result from unit test (eg: rename A220-100 to A220-400)  
+  Re-run pytest
--- python/02_python-basics/labs/tp3_solution.py
+++ python/02_python-basics/labs/tp3_solution.py
@@ -2,7 +2,8 @@ from datetime import datetime
 from zoneinfo import ZoneInfo
 from time import sleep
 import json
-
+import csv
+import os
 
 print ('TP N°3 (Python Basics)')
 print('Functions')
@@ -87,8 +88,8 @@ def simple_clock_even_odd(duration: int = 6):
         sleep(1)
 
 
-#simple_clock()
-#simple_clock_even_odd(5)
+simple_clock()
+simple_clock_even_odd(5)
 
 
 def simple_timer(duration: int = 12):
@@ -126,3 +127,94 @@ def save_as_json(**kwargs):
 
 
 save_as_json(plane_1='A220', plane_2='A320', plane_3='A330', plane_4='A350')
+
+
+
+print('3.3.2')
+with open('tp3_airbus_family.csv') as f:
+    reader = csv.reader(f, delimiter=',')
+    next(reader)  # skip header
+    for line in reader:
+        print(line)
+
+print('3.3.3')
+titles = ['name', 'seating', 'range']
+with open('tp3_airbus_family.csv') as f:
+    reader = csv.DictReader(f, titles)
+    next(reader)  # skip header
+    for line in reader:
+        print(line)
+
+# 3.3.4
+
+print('3.3.4a')
+titles = ['name', 'seating', 'range']
+aircrafts = []
+with open('tp3_airbus_family.csv') as infile:
+    reader = csv.DictReader(infile, titles)
+    next(reader)  # skip header
+    for line in reader:
+        aircrafts.append({'name': line['name'], 'seating': line['seating'], 'range': line['range']})
+with open('tp3_airbus_family.json', 'w') as outfile:
+    outfile.write(json.dumps(aircrafts))
+
+print('3.3.4b')
+titles = ['name', 'seating', 'range']
+with open('tp3_airbus_family.csv') as infile:
+    reader = csv.DictReader(infile, titles)
+    next(reader)  # skip header
+    with open('tp3_airbus_family.json', 'w') as outfile:
+        first_line = True
+        outfile.write('[\n')
+        for line in reader:
+            if not first_line:
+                outfile.write(',\n')
+            outfile.write(json.dumps({'name': line['name'], 'seating': line['seating'], 'range': line['range']}, indent=4))
+            first_line = False
+        outfile.write('\n]')
+
+# 3.4 Test
+
+print('3.4.1')
+def aircrafts_csv_to_json(csvin: str, jsonout: str):
+    titles = ['name', 'seating', 'range']
+    aircrafts = []
+    with open(csvin) as infile:
+        reader = csv.DictReader(infile, titles)
+        next(reader)  # skip header
+        for line in reader:
+            aircrafts.append({'name': line['name'], 'seating': line['seating'], 'range': line['range']})
+    with open(jsonout, 'w') as outfile:
+        outfile.write(json.dumps(aircrafts))
+
+
+print('3.4.2')
+def test_aircrafts_csv_to_json():
+    jsontest = 'tp3_conversion-test.json'
+    aircrafts_csv_to_json(csvin='tp3_airbus_family.csv', jsonout=jsontest)
+    expected_list = [
+        {'name': 'A220-100', 'seating': '100-120', 'range': '3450'},
+        {'name': 'A220-300', 'seating': '120-150', 'range': '3400'},
+        {'name': 'A319neo', 'seating': '120-150', 'range': '3650'},
+        {'name': 'A320neo', 'seating': '150-180', 'range': '3450'},
+        {'name': 'A321neo', 'seating': '180-220', 'range': '4000'},
+        {'name': 'A321XLR', 'seating': '180-220', 'range': '4700'},
+        {'name': 'A330-800', 'seating': '220-260', 'range': '8150'},
+        {'name': 'A330-900', 'seating': '260-300', 'range': '7200'},
+        {'name': 'A350-900', 'seating': '300-350', 'range': '8100'},
+        {'name': 'A350-1000', 'seating': '350-410', 'range': '8700'},
+        {'name': 'A380', 'seating': '400-550', 'range': '8000'}
+    ]
+
+    with open(jsontest) as infile:
+        read_list = json.load(infile)  # read_list = json.loads(infile.read())
+        assert read_list == expected_list
+
+    if os.path.exists(jsontest):
+        os.remove(jsontest)
+
+
+print('3.4.3')
+if __name__ == "__main__":
+    test_aircrafts_csv_to_json()
+    print("test Ok")
