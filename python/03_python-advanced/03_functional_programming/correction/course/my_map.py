1bc3a8c52c41d5fc6a2e03ae9f750b67e09aac0e
bondouy_m <manon.bondouy@airbus.com>
2022-09-14 17:50:24 +0200

    delete functions

--- python/03_python-advanced/03_functional_programming/correction/course/my_map.py
+++
@@ -1,9 +0,0 @@
-from typing import List, Iterable
-def my_map(list_: List, function)->List:
-    def my_map_len(input_list, len_input_list, function):
-        if len_input_list == 0:
-            return []
-        else:
-            return [function(input_list[0])]+my_map_len(input_list[1:], len_input_list-1, function)
-    return my_map_len(list_, len(list_), function)
-
