046a33f2b09c853bdc95c5ff6bac4d8d75cf1411
bondouy_m <manon.bondouy@airbus.com>
2022-09-14 17:55:49 +0200

    delete factorial map

--- python/03_python-advanced/03_functional_programming/correction/course/factorial_map.py
+++
@@ -1,9 +0,0 @@
-factorials = []
-def f(x):
-    if x == 0:
-        factorials.append(1)
-        return
-    factorials.append(factorials[-1]*x)
-li = list(range(10))
-my_map(li, f)
-print(factorials)
