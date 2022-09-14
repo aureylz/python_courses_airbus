8517ef229791e5dd372fc0c809e547cd6bd392d9
bondouy_m <manon.bondouy@airbus.com>
2022-09-14 17:58:16 +0200

    list recursive delete

--- python/03_python-advanced/03_functional_programming/correction/course/print_list_recursive.py
+++
@@ -1,6 +0,0 @@
-def print_list_recursive(list_of_something: List[Any]):
-    if len(list_of_something) == 0:
-        return                     
-    else:
-        print(list_of_something[0])       
-        return print_list_recursive(list_of_something[1:])  
