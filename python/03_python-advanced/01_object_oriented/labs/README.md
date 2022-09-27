941f458fd2864663cee8fc68eda4d2c49e8cfb0f
Sebastien Arfort <sebastien.arfort@airbus.com>
2022-09-27 20:06:49 +0200

    add new (simpler) OOP lab

--- python/03_python-advanced/01_object_oriented/labs/README.md
+++ python/03_python-advanced/01_object_oriented/labs/README.md
@@ -1,149 +1,111 @@
-# Oriented Object Hands-On
+# Python/Object Oriented/Labs
 
-In this lab, we'll be leveraging OOP to implement a versatile [Genetic Algorithm](https://en.wikipedia.org/wiki/Genetic_algorithm) that can be used to soleve differents problems.
+## Problem statement
 
-As per Wikipedia:
+You've been hired as the lead developer in a new bank.
 
-> G-A are inspired by the process of natural selection that belongs to the larger class of evolutionary algorithms (EA). 
+Your mission is to implement the skeleton of the software that will manage customers banking accounts.
+
+Nowadays, our bank provides the customers with 3 account types:
+
+- ```CurrentAccount```
+- ```SavingAccount```
+- ```CreditCardAccount```
+
+They all allow you to ```deposit``` and ```withdraw``` money, but with slightvariations described beneath.
+
+Once an account has been closed, no further transactions will be allowed on the latter.
+
+Every customer can request a copy of its account ```history``` (Relevé des opérations)
+
+> But, be sure that our __marketing__ colleagues from the up floor are already working on plethora of account variations to catch even more customers in the the coming months.
 >
-> Genetic algorithms are commonly used to generate high-quality solutions to optimization and search problems by relying on biologically inspired operators such as mutation, crossover and selection
+> Thus, your software design must indeed take this fact into account and _automagically_ adapt to the upcoming offers (new account types) with only a minimal coding / re-testing effort.
 
-This is a non-deterministic way to find acceptable solutions in a minimum amount of time for complex problems.
+### The ```CurrentAccount```
 
-For that to work, the problem you're trying to solve must be modelizable with chromosoms and there must be a way to evaluate a given chromosom to 'measure' how far it is from the optimal solution.
+It's the most basic (Compte Courant) and allows a negative balance up to -100 (for all customers)
 
-## Available material
+### The ```SavingAccount```
 
-Because the whole purpose of this lab is to manipulate classes and objects and not focusing on algorithm implementation, we are providing you with some existing code:
+Is a ```CurrentAccount``` which also serves customer negociated yearly interests (around 2%).
 
-- [genetic_utils.py](genetic_utils.py) implements the mutation, crossover and selection operators; it also contains a full implementation of the genetic algorithm that uses those operators.
-- [main.py](main.py) uses the above tools to find a solution to the OneMax problem.
+(Un pseudo Livret A, mais chaque client peut légèrement discuter son taux à l'ouverture...)
 
-All this code is working but is implemented in the form of functions and doesn't use the power of the OOP.
+### The ```CreditCardAccount```
 
-We want to make it more generic, so that, the same code can be used to solve different kind of problems with just implementing a new class that models the problem to solve.
+It delays payments by N days (negiciated at opening for each customer as for the above interest rates...).
 
-### Fitness function
+Unlike ```CurrentAccount```, there is no limit to negative balance (découvert), however, the customer agrees to pay 18% of interests on negative balance...
 
-It tells how good is a given citizen when it comes to a specific problem resolution.
+(Carte Bleue à débits différés)
 
-Here, in the OneMax challenge, the fitness function is just the sum of the ones.
+## Excpected result
 
-### Genetic operators
+We don't know how to produce the necessary code to achieve that, but that's your job.
 
-It mimics the genetic on natural entities (humans, plants, animals...)
+But, at the end of the day, we want to be able to run those bits of code to integrate that with the rest of our Information System:
 
-#### Mutation
+### CurrentAccount
 
-At random rate, a mutation can occur, a mutation is simply a bit swap in a DNA chain, for instance:
-
-```100101001``` => ```000101001``` (here the first bit changed from 1 to 0)
-
-#### Cross-Over
-
-This models a basic reproduction process in which 2 parents will generate 2 childs.
-The childs DNA chain will be composed of some of the DNA from parent 1 and some DNA from parent 2:
-
-To achieve this, the cross-over operator will choose a random position in the DNA chain, everything before this index will be taken from parent 1 and the rest from parent 2:
-
-Example:
-
-Let say our split index will be 3, which means, we'll be taking the first 3 chromosoms from parent 1 and the rest from parent 2.
-
-```11111111```: Parent 1 DNA
-
-```00000000```: Parent 2 DNA
-
-```___|____```: Split index / position
-
-```11100000```: Child DNA
-
-#### Selection
-
-Is a kind of tournament to decide which citizen in the population will become reproducers.
-The idea is to flavor the citizen who are the more adaptated to their environment (have the best fitness function).
-
-#### The G-A algo
-
-Will put all the above concepts together (genetic operators, fitness....) on a random population and will compute several new generations to try to converge on one acceptable solution.
-
-## The One Max problem
-
-In the OneMax challenge, the goal is to get maximize the amount of 1 in a list of boolen values.
-
-The list of O and 1 will form the DNA of one citizen in our population.
-
-For instance, if we consider a 8 bits genoma:
-
-```100101001``` is one possible DNA chain, ```110100101``` is another one.
-
-```11111111``` contains 8 times the digit ```1``` and is the best solution we can find with an 8 bits length DNA chain.
-
-### Sample runs
-
-Here we're using a 32 bits size genome.
-Mutiple runs will lead to different ways of browsing the entire search space.
-The random nature of mutations and cross-over is what brings diversity.
-The selection operator helps converging to an acceptable solution.
-Some parameters can be changed to influence the internal behavior like mutation or cross-over rate, the amount of citizens in the population, the size of the genome...
-
-#### Run #1
-
-```
-Generation #:0
-	Found a better candidate citizen (fitness=  18 and DNA=10101001100011011101100100111110)
-	Found a better candidate citizen (fitness=  19 and DNA=00111011100101011111110001010011)
-	Found a better candidate citizen (fitness=  20 and DNA=11010100010110111001111101110110)
-	Found a better candidate citizen (fitness=  21 and DNA=10010110001111111001111101110101)
-	Found a better candidate citizen (fitness=  22 and DNA=10110111111101101001011110110011)
-Generation #:1
-	Found a better candidate citizen (fitness=  23 and DNA=01110110001111111001111101110111)
-	Found a better candidate citizen (fitness=  24 and DNA=11111111100111011010001101111111)
-Generation #:2
-	Found a better candidate citizen (fitness=  25 and DNA=11111111110111011010101110111011)
-Generation #:3
-	Found a better candidate citizen (fitness=  26 and DNA=11101111110111111011001111111110)
-	Found a better candidate citizen (fitness=  27 and DNA=11111111110111111011001111111110)
-Generation #:4
-	Found a better candidate citizen (fitness=  29 and DNA=11111111011110111111111101111111)
-Generation #:5
-	Found a better candidate citizen (fitness=  30 and DNA=11111111111110111111111101111111)
-Generation #:6
-	Found a better candidate citizen (fitness=  31 and DNA=11111111111111111110111111111111)
-Generation #:7
-Generation #:8
-Generation #:9
-	Found a better candidate citizen (fitness=  32 and DNA=11111111111111111111111111111111)
-I guess I found an acceptable solution (algorithm converged in 9 generations)
-Done!
-Fitness 32 => DNA=11111111111111111111111111111111
+```python
+    airbus_current = CurrentAccount(owner='Airbus', initial_balance=10000.0)
+    # to mimic different datetimes...
+    sleep(1)
+    airbus_current.deposit(300.0, 'Pre-sale for a plane, customer X pays 10% for booking')
+    sleep(1)
+    airbus_current.withdraw(15.0, 'Furniture for the factories')
+    sleep(1)
+    airbus_current.withdraw(37.0, 'Engine purchase')
+    sleep(1)
+    airbus_current.withdraw(19.0, 'Electrical fees for the factories')
+    sleep(1)
+    airbus_current.deposit(2700.0, 'Plane delivered, customer Y pays 90% at reception')
+    print(airbus_current.history_fancy)
 ```
 
-#### Run #2
+### SavingAccount
 
+```python
+    my_saving_account = SavingAccount(owner='Seb', initial_balance=100.0)
+    my_saving_account.give_yearly_interests()
+    for month in range(12):
+        my_saving_account.deposit(400.0, f'Pay day for month {month} !')
+    my_saving_account.give_yearly_interests()
+    my_saving_account.withdraw(2000.0, 'Buy new laptop')
+    my_saving_account.withdraw(2750.0, 'Buy new loudspeakers')
+    try:
+        my_saving_account.withdraw(10000.0, 'Why not also buy this new fancy car?')
+    except PermissionError:
+        print('Argh, I would need more money to get that car!')
+    my_cash = my_saving_account.close('I no longer need a saving account :)')
+    print(f'I got {my_cash:,.2f} in cash!')
+    try:
+        my_saving_account.withdraw(100, 'Attempt to get money from a closed account :)')
+    except PermissionError as ex:
+        print(ex.args[0])
+    print(my_saving_account.history_fancy)
 ```
-Generation #:0
-	Found a better candidate citizen (fitness=  18 and DNA=11111101000101011111000100001011)
-	Found a better candidate citizen (fitness=  19 and DNA=10011100111100010111001110001111)
-	Found a better candidate citizen (fitness=  22 and DNA=11011110100100111101011011101111)
-	Found a better candidate citizen (fitness=  23 and DNA=10011110001011111110111111110011)
-Generation #:1
-	Found a better candidate citizen (fitness=  27 and DNA=11111101111011111110111111110011)
-Generation #:2
-Generation #:3
-	Found a better candidate citizen (fitness=  28 and DNA=11111101111101111111111101101111)
-Generation #:4
-Generation #:5
-	Found a better candidate citizen (fitness=  29 and DNA=11111111111101110111111101111111)
-Generation #:6
-Generation #:7
-Generation #:8
-	Found a better candidate citizen (fitness=  30 and DNA=11111101111011111111111111111111)
-	Found a better candidate citizen (fitness=  31 and DNA=11111101111111111111111111111111)
-Generation #:9
-Generation #:10
-	Found a better candidate citizen (fitness=  32 and DNA=11111111111111111111111111111111)
-I guess I found an acceptable solution (algorithm converged in 10 generations)
-Done!
-Fitness 32 => DNA=11111111111111111111111111111111
+
+### CreditCardAccount
+
+
+```python
+    my_credit_card = CreditCardAccount(owner='Seb', initial_balance=200.0)
+    try:
+        # Try to get interest from this account type, who knows, it might work...
+        my_credit_card.give_yearly_interests()
+    except AttributeError as ex:
+        print(ex.args[0])
+    my_credit_card.withdraw(6.35, 'Canteen')
+    my_credit_card.withdraw(189.0, 'Beers with the tam')
+    my_credit_card.withdraw(120.0, 'Fuel for the car')
+    try:
+        my_credit_card.withdraw(10000.0, "With that account I can get that car, but I'll pay massive debt fees...")
+    except PermissionError:
+        # that cannot happen because CreditCardAccount allows the payment
+        # even if the balance isn't sufficient to cover the purchase...
+        pass
+    my_credit_card.take_debt_fees()
+    print(my_credit_card.history_fancy)
 ```
---
+++ python/03_python-advanced/01_object_oriented/labs/labs_solution.py
@@ -0,0 +1,305 @@
+from abc import ABC, abstractmethod
+from collections import namedtuple
+from datetime import datetime, timedelta
+import json
+import logging
+from time import sleep
+from typing import Union
+from uuid import uuid4
+
+TransactionDetails = namedtuple('TransactionDetails', ['at_date', 'amount', 'description'])
+
+
+class BankAccountBase(ABC):
+    """
+    This is the 'base' account class that act as a common ancestor to all account types.
+    It ensures all inherited account classes will have at least those properties and methods available.
+    Notice that it's marked as abstract (because it inherits from ABC).
+    An abstract class is not meant to be instantiated:
+    my_account = BankAccountBase('seb', 49.99) makes no sense in the real world because the bank
+    has no such account type in its products catalog (only CurrentAccount, SavingAccount...).
+    The child class is obliged to implement (code) the method withdraw() to cover it's specific needs.
+    Because the withdraw rules will differ depending on the account type we mark it as @abstractmethod.
+    Child classes can also (but are not obliged to) customize (override) the ancestor's methods to specialize them.
+    """
+
+    def __init__(self, owner: str, initial_balance: float):
+        self._balance: float = 0.0
+        self._owner: str = owner
+        self._account_id: str = str(uuid4())
+        self._created_at: datetime = datetime.now()
+        self._closed_at: Union[datetime, None] = None
+        self._close_reason: Union[str, None] = None
+        self._transactions: list[TransactionDetails] = []
+        if initial_balance > 0:
+            logging.info(f'{self._owner} {type(self).__name__} created ({self._account_id}), initial balance={self._balance:,.2f}')
+            self.deposit(initial_balance, 'Initial balance')
+        else:
+            logging.error(f'{self._owner} {type(self).__name__} creation rejected, no money provided!')
+            raise ValueError("if you have no money, then you don't need a bank account...")
+
+    def deposit(self, amount: float, description: str = None) -> float:
+        """
+        This methods puts money on the account.
+        :param amount: the quantity of money to put on the account
+        :param description: An optional description for the transaction
+        :return: the new account balance
+        """
+        if self._closed_at is not None:
+            logging.error(f'Transaction attempt on a closed account: {self._owner} tried to put {amount:,.2f} on {type(self).__name__} ({self._account_id})')
+            raise PermissionError("It's forbidden to perform a transaction on a closed account")
+        else:
+            self._transactions.append(TransactionDetails(at_date=datetime.now(),
+                                                         amount=amount,
+                                                         description=description if description is not None else '<No description>'))
+            self._balance += float(amount)
+            logging.info(f'{self._owner} put {amount:,.2f} on {type(self).__name__} ({self._account_id}), new balance={self._balance:,.2f}')
+            return self._balance
+
+    @abstractmethod
+    def withdraw(self, amount: float, description: str = None) -> float:
+        """
+        This methods takes money from the account.
+        This abstract method is meant to be implement in the child classes (mandatory).
+        This is the responsibility of the implementer to ensure the account isn't closed before performing the transaction!
+        :param amount: the quantity of money to take from the account
+        :param description: An optional description for the transaction
+        :return: the new account balance
+        """
+        ...
+
+    @property
+    def history(self) -> list[TransactionDetails]:
+        """
+        A copy of the transactions log on the account
+        :return: a list of TransactionDetails (named tuple)
+        """
+        logging.info(f'Audit: History request for {self._owner} {type(self).__name__} ({self._account_id})')
+        result = self._transactions.copy()
+        result.reverse()
+        return result
+
+    @property
+    def history_fancy(self) -> str:
+        """
+        The transactions logs of the account plus some important account properties (balance, owner, id...)
+        :return: A formatted string with the above information (suitable for printing to console)
+        """
+        lines: list[str] = []
+        logging.info(f'Audit: History request for {self._owner} {type(self).__name__} ({self._account_id})')
+        lines.extend(list(f'{k:50}:{v}' for k, v in self.details.items()))
+        lines.append(f'Balance                                           :{self._balance:,.2f}')
+        for transaction in self._transactions:
+            if transaction.amount < 0:
+                lines.append(f'{transaction.at_date:%Y-%m-%d %H:%M:%S} | {transaction.description:80} | {20 * " "} | {float(transaction.amount):>20,.2f}')
+            else:
+                lines.append(f'{transaction.at_date:%Y-%m-%d %H:%M:%S} | {transaction.description:80} | {float(transaction.amount):>20,.2f} |  {20 * " "}')
+        return '\n'.join(lines)
+
+    def close(self, reason: str) -> float:
+        """
+        Definitely closes the account
+        :param reason:
+        :return:
+        """
+        logging.info(f'{self._owner} {type(self).__name__} ({self._account_id}) is being closed')
+        self._closed_at = datetime.now()
+        self._close_reason = reason
+        closure_balance = self._balance
+        self._balance = 0
+        return closure_balance
+
+    @property
+    def balance(self) -> float:
+        return self._balance
+
+    @property
+    def owner(self) -> str:
+        return self._owner
+
+    @property
+    def account_id(self) -> str:
+        return self._account_id
+
+    @property
+    def details(self) -> dict[str, str]:
+        if self._closed_at is not None:
+            return {
+                'id': self._account_id,
+                'owner': self._owner,
+                'created_at': self._created_at,
+                'closed_at': self._closed_at,
+                'close_reason': self._close_reason,
+            }
+        else:
+            return {
+                'id': self._account_id,
+                'owner': self._owner,
+                'created_at': self._created_at,
+                'balance': self._balance,
+                'report_date': datetime.now(),
+            }
+
+
+class CurrentAccount(BankAccountBase):
+    """
+    This is the most used and known account type that everyone has.
+    It just supports standard deposits and withdraws.
+    The min balance allow (debt clearance) is 100.00 for all customers (class variable)
+    """
+    _debt_clearance = -100.0
+
+    def __init__(self, owner: str, initial_balance):
+        super(CurrentAccount, self).__init__(owner, initial_balance)
+
+    def withdraw(self, amount: float, description: str = None) -> float:
+        if self._closed_at is not None:
+            logging.error(f'Transaction attempt on a closed account: {self._owner} tried to get {amount:,.2f} from {type(self).__name__} ({self._account_id})')
+            raise PermissionError("It's forbidden to perform a transaction on a closed account")
+        else:
+            if (self._balance - amount) >= CurrentAccount._debt_clearance:
+                self._transactions.append(TransactionDetails(at_date=datetime.now(), amount=-amount, description=description))
+                self._balance -= amount
+                logging.info(f'{self._owner} get {amount:,.2f} from {type(self).__name__} created ({self._account_id}), new balance={self._balance:,.2f}')
+                return self._balance
+            else:
+                logging.error(f'{self._owner} cannot get {amount:,.2f} from {type(self).__name__} ({self._account_id})!')
+                raise PermissionError(f'You cannot withdraw that amount of money: debt clearance exhausted !')
+
+
+class SavingAccount(BankAccountBase):
+    """
+    This account serves a yearly interest rate.
+    There's no debt clearance (the account balance remains stricly positive at any time).
+    The interest rate is defined at creation and can't be changed afterwards,
+    but each customer can negotiate a different rate while opening (instance variable).
+    """
+    def __init__(self, owner: str, initial_balance: float, interest_rate: float = .02):
+        super(SavingAccount, self).__init__(owner, initial_balance)
+        self._interest_rate = interest_rate
+
+    def withdraw(self, amount: float, description: str = None) -> float:
+        if self._closed_at is not None:
+            logging.error(f'Transaction attempt on a closed account: {self._owner} tried to get {amount:,.2f} from {type(self).__name__} ({self._account_id})')
+            raise PermissionError("It's forbidden to perform a transaction on a closed account")
+        if amount - self._balance < 100:
+            self._transactions.append(TransactionDetails(at_date=datetime.now(), amount=-amount, description=description))
+            self._balance -= amount
+            logging.info(f'{self._owner} get {amount:,.2f} from {type(self).__name__} created ({self._account_id}), new balance={self._balance:,.2f}')
+            return self._balance
+        else:
+            logging.error(f'{self._owner} cannot get {amount:,.2f} from {type(self).__name__} ({self._account_id})!')
+            raise PermissionError(f'You cannot withdraw that amount of money: debt clearance exhausted!')
+
+    def give_yearly_interests(self) -> None:
+        earned_interests = self._balance * self._interest_rate
+        self.deposit(earned_interests, 'Yearly interests')
+        logging.debug(f'{self._owner} receives {earned_interests:,.2f} of interests on {type(self).__name__} ({self._account_id}) new balance={self._balance:,.2f}')
+
+
+class CreditCardAccount(BankAccountBase):
+    """
+    This account allows negative balance, but in that case, the customer agrees to pay a legal 18% debt fees.
+    The payments are also delayed by a certain amount of days that can be negotiated at opening.
+    """
+    _debt_rate = .18
+
+    def __init__(self, owner: str, initial_balance: float, payment_delay_days: int = 30):
+        super(CreditCardAccount, self).__init__(owner, initial_balance)
+        self._payment_delay_days = payment_delay_days
+
+    def withdraw(self, amount: float, description: str = None) -> float:
+        if self._closed_at is not None:
+            logging.error(f'Transaction attempt on a closed account: {self._owner} tried to get {amount:,.2f} from {type(self).__name__} ({self._account_id})')
+            raise PermissionError("It's forbidden to perform a transaction on a closed account")
+        self._transactions.append(TransactionDetails(
+            at_date=datetime.now() + timedelta(days=self._payment_delay_days),
+            amount=-amount,
+            description=description))
+        self._balance -= amount
+        logging.info(f'{self._owner} get {amount:,.2f} from {type(self).__name__} ({self._account_id}), new balance={self._balance:,.2f}')
+        return self._balance
+
+    def take_debt_fees(self) -> None:
+        if self._balance < 0:
+            debt_fees = -self._balance * self._debt_rate
+            logging.debug(f'{self._owner} pays {debt_fees:,.2f} on {type(self).__name__} ({self._account_id}) because balance={self._balance:,.2f}')
+            self.withdraw(debt_fees, 'Debt fees')
+
+
+def demo_current_account(bank_name: str):
+    print(f'** {bank_name} **')
+    airbus_current = CurrentAccount(owner='Airbus', initial_balance=10000.0)
+    sleep(1)
+    airbus_current.deposit(300.0, 'Pre-sale for a plane, customer X pays 10% for booking')
+    sleep(1)
+    airbus_current.withdraw(15.0, 'Furniture for the factories')
+    sleep(1)
+    airbus_current.withdraw(37.0, 'Engine purchase')
+    sleep(1)
+    airbus_current.withdraw(19.0, 'Electrical fees for the factories')
+    sleep(1)
+    airbus_current.deposit(2700.0, 'Plane delivered, customer Y pays 90% at reception')
+    print(airbus_current.history_fancy)
+
+
+def demo_saving_account(bank_name: str):
+    print(f'** {bank_name} **')
+    my_saving_account = SavingAccount(owner='Seb', initial_balance=100.0)
+    my_saving_account.give_yearly_interests()
+    for month in range(12):
+        my_saving_account.deposit(400.0, f'Pay day for month {month} !')
+    my_saving_account.give_yearly_interests()
+    my_saving_account.withdraw(2000.0, 'Buy new laptop')
+    my_saving_account.withdraw(2750.0, 'Buy new loudspeakers')
+    try:
+        my_saving_account.withdraw(10000.0, 'Why not also buy this new fancy car?')
+    except PermissionError:
+        print('Argh, I would need more money to get that car!')
+    my_cash = my_saving_account.close('I no longer need a saving account :)')
+    print(f'I got {my_cash:,.2f} in cash!')
+    try:
+        my_saving_account.withdraw(100, 'Attempt to get money from a closed account :)')
+    except PermissionError as ex:
+        print(ex.args[0])
+    print(my_saving_account.history_fancy)
+
+
+def demo_credit_account(bank_name: str):
+    print(f'** {bank_name} **')
+    my_credit_card = CreditCardAccount(owner='Seb', initial_balance=200.0)
+    try:
+        # Try to get interest from this account type, who knows, it might work...
+        my_credit_card.give_yearly_interests()
+    except AttributeError as ex:
+        print(ex.args[0])
+    my_credit_card.withdraw(6.35, 'Canteen')
+    my_credit_card.withdraw(189.0, 'Beers with the tam')
+    my_credit_card.withdraw(120.0, 'Fuel for the car')
+    try:
+        my_credit_card.withdraw(10000.0, "With that account I can get that car, but I'll pay massive debt fees...")
+    except PermissionError:
+        # that cannot happen because CreditCardAccount allows the payment
+        # even if the balance isn't sufficient to cover the purchase...
+        pass
+    my_credit_card.take_debt_fees()
+    print(my_credit_card.history_fancy)
+
+
+if __name__ == '__main__':
+    # Change me to see more verbose logs !
+    # logging.basicConfig(level=logging.DEBUG)
+    # logging.basicConfig(level=logging.INFO)
+    logging.basicConfig(level=logging.WARNING)
+
+    # Challenge for the bravest...
+    # In this version, we trust the implementers of the child classes enough.
+    # But, a faulty implementation could possibly (real bug or bad guys):
+    # - withdraw money from a closed account...
+    # - update or delete existing transactions from the history...
+    # To prevent those attacks, in the base class:
+    # - turn the self._transactions from protected to private: self.__transactions (a __ prefix means private)
+    # - add a protected method so that child classes can ONLY append a transaction to your private self.__transactions
+    demo_saving_account('My Online Bank')
+    demo_credit_account('My Credit Card')
+    demo_current_account('European Central Bank')
